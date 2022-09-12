966c30ed424fe5ffcb51e4ca9017be5429e98c07
Bilal Gurani <bilal.gurani@utopusinsights.com>
2022-09-12 12:55:29 +0530

    Add bootstrap and firebase modules

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,16 @@
 
 export const environment = {
   production: false,
-  apiKey: "AIzaSyBnzFL-TCghg7WVOY5vSBn39eqDvq-n1ug",
-  authDomain: "eshop-ed60e.firebaseapp.com",
-  projectId: "eshop-ed60e",
-  storageBucket: "eshop-ed60e.appspot.com",
-  messagingSenderId: "248305421094",
-  appId: "1:248305421094:web:9be820f81092f72aca4fe7",
-  measurementId: "G-8YPB207JD9"
+  firebase: {
+    apiKey: "AIzaSyBnzFL-TCghg7WVOY5vSBn39eqDvq-n1ug",
+    authDomain: "eshop-ed60e.firebaseapp.com",
+    projectId: "eshop-ed60e",
+    storageBucket: "eshop-ed60e.appspot.com",
+    messagingSenderId: "248305421094",
+    appId: "1:248305421094:web:9be820f81092f72aca4fe7",
+    measurementId: "G-8YPB207JD9"
+  }
+
 };
 
 /*
